0cc8eb95ad1a9cc1315d4d53b5b527dc44c64303
BABIGONZ <113170064+pitchayut46@users.noreply.github.com>
2023-03-27 20:47:33 +0700

    a

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import"./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/IMG_3678.jpg" alt="Avatar"/>
+            <h1 className="name">Pitchayut B</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
